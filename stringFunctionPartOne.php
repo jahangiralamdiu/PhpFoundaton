3e47289a313607d2b1e4bb8bf5b159d7f7a58435
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-07-09 19:59:59 +0600

    Add chr() function

---
+++ stringFunctionPartOne.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Function name chr($ascii)
+
+$str = "The String is ended by : ";
+
+$str.=chr(65);
+
+echo $str;
