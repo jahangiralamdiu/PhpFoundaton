0cdb945be40fb3763a650075df68a5b25fb5da87
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-07-09 22:18:16 +0600

    array_column

--- arrayFunctionsPartOne.php
+++ arrayFunctionsPartOne.php
@@ -14,3 +14,30 @@ print_r(array_change_key_case($input_array, CASE_UPPER));
 $input_array2 = array('a', 'b', 'c', 'd', 'e');
 print_r(array_chunk($input_array2, 2));
 print_r(array_chunk($input_array2, 2, true));
+
+//Function array_column()
+$records = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    )
+);
+ 
+$first_names = array_column($records, 'first_name');
+print_r($first_names);
