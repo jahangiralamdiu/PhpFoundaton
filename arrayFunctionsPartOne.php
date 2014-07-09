7898dfcf19f3ae2791d5d3e2ecd8e3b5c045ae80
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-07-09 22:58:57 +0600

    array_diff_assoc

--- arrayFunctionsPartOne.php
+++ arrayFunctionsPartOne.php
@@ -56,3 +56,10 @@ $array = array(1, "hello", 1, "world", "hello");
 print_r(array_count_values($array));
 
 echo "<br/>";
+
+//Function array_diff_assoc
+
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+$result = array_diff_assoc($array1, $array2);
+print_r($result);
