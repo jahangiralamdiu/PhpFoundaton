59c5b1b012c14dda9d3c8709af98a78ba46c643b
towfiqpiash <towfiqpiash@gmail.com>
2014-07-09 14:25:00 +0600

    Worng input

--- gradeSheet.php
+++ gradeSheet.php
@@ -5,39 +5,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$mark =43;
+$mark =112;
 
 if($mark<=32)
 {
     echo "The grade is F";
 }
 
-if($mark<40 && $mark>=33)
+else if($mark<40 && $mark>=33)
 {
     echo "The grade is D";
 }
 
-if($mark<50 && $mark>=40)
+else if($mark<50 && $mark>=40)
 {
     echo "The grade is C";
 }
 
-if($mark<60 && $mark>=50)
+else if($mark<60 && $mark>=50)
 {
     echo "The grade is B";
 }
 
-if($mark<70 && $mark>=60)
+else if($mark<70 && $mark>=60)
 {
     echo "The grade is A-";
 }
 
-if($mark<80 && $mark>=70)
+else if ($mark<80 && $mark>=70)
 {
     echo "The grade is A";
 }
 
-if($mark<=100 && $mark>=80)
+else if($mark<=100 && $mark>=80)
 {
     echo "The grade is A+";
+}
+ else {
+     echo "Wrong input";
 }
