50493a168f8e71254592c74a2cd5ea124b0a8452
towfiqpiash <towfiqpiash@gmail.com>
2014-07-09 11:46:58 +0600

    Math Operator

---
+++ operators.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$x = 100/(2*5);
+$y=100%(2*5);
+$z = 100/2*5;
+$a = 0/5;
+$b=0%5;
+
+echo $x."<br />";
+echo $y."<br />";
+echo $z."<br />";
+echo $a."<br />";
+echo $b."<br />";
+
+
